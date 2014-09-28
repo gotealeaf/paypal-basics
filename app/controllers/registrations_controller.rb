9f96b2b95981dad82f5b3a056014d2cb13448907
Juan Lebrijo <juan@lebrijo.com>
2014-09-28 15:14:50 +0200

    Step 2. Redirecting to Paypal url

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -19,15 +19,10 @@ class RegistrationsController < ApplicationController
   # POST /registrations
   def create
     @registration = Registration.new(registration_params)
-
-    respond_to do |format|
-      if @registration.save
-        format.html { redirect_to @registration, notice: 'Registration was successfully created.' }
-        format.json { render :show, status: :created, location: @registration }
-      else
-        format.html { render :new }
-        format.json { render json: @registration.errors, status: :unprocessable_entity }
-      end
+    if @registration.save
+      redirect_to @registration.paypal_url(registration_path(@registration))
+    else
+      render :new
     end
   end
 
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,22 @@
 class Registration < ActiveRecord::Base
   belongs_to :course
+
+  validates :full_name, :company, :email, :telephone, presence: true
+
+  def paypal_url(return_url)
+    values = {
+        business: "merchant@gotealeaf.com",
+        cmd: "_xclick",
+        upload: 1,
+        return: return_url,
+        invoice: id,
+        amount: course.price,
+        item_name: course.name,
+        item_number: course.id,
+        quantity: '1'
+    }
+    "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+  end
+
+
 end
