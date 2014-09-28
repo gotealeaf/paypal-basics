b27fe07867f0faf33c15a00714f2f7a6a71c231d
Juan Lebrijo <juan@lebrijo.com>
2014-09-28 16:02:29 +0200

    Step 4. ngrok needed fixes

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,18 +3,18 @@ class Registration < ActiveRecord::Base
 
   validates :full_name, :company, :email, :telephone, presence: true
 
-  def paypal_url(return_url)
+  def paypal_url(return_path)
     values = {
         business: "merchant@gotealeaf.com",
         cmd: "_xclick",
         upload: 1,
-        return: return_url,
+        return: "#{Rails.application.secrets.app_host}#{return_path}",
         invoice: id,
         amount: course.price,
         item_name: course.name,
         item_number: course.id,
         quantity: '1',
-        notify_url: "#{Rails.application.secrets.app_host}/cgi-bin/"
+        notify_url: "#{Rails.application.secrets.app_host}/hook"
     }
     "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
   end
